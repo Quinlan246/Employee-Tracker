1b39759fa872cdbfbbc202990c52416530dedab9
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-10-15 10:41:16 -0500

    added schema and seeds files

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'DB_USER',
+        password: 'DB_PASSWORD',
+        database: 'employees_db',
+    },
+    console.log('connected to the employee database')
+);
+
+module.exports = db
+
