375286a8583e21261c53451ecd2fb11879f017a2
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-10-16 00:11:26 -0500

    added show functions

--- server.js
+++ server.js
@@ -76,11 +76,51 @@ const promptUser = () => {
     })
 }
 
-showDepartments()
+showDepartments = () => {
+    console.log('Showing all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
 
-showRoles()
+    Connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
 
-showEmployees()
+showRoles = () => {
+    console.log('Showing all roles...\n');
+
+    const sql = `SELECT role.id, role.title, department.name AS department
+                FROM role
+                INNER JOIN department ON role.department_id = department.id`
+
+    Connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+showEmployees = () => {
+    console.log('Showing all employees...\n');
+    const sql = `SELECT employee.id,
+                        employee.first_name,
+                        employee.last_name,
+                        role.title,
+                        department.name AS department,
+                        role.salary,
+                        CONCAT (manager.first_name, ' ', manager.last_name) AS manager
+                    FROM employee
+                        LEFT JOIN role ON employee.role_id = role.id
+                        LEFT JOIN department ON role.department_id = department.id
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id`
+
+    Connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser;
+    });
+};
 
 addDepartment()
 
@@ -94,6 +134,6 @@ updateManager()
 
 deleteDepartment()
 
-deleteRole()
+deleteRole() 
 
 deleteEmployee()
